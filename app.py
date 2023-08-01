0b333b0f583562f54b7128b929be1ecff02e0825
ZikaFatmaAulia <zikafatmaa05@gmail.com>
2023-08-01 11:03:29 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,8 @@
+# hitung Luas Segitiga aederhana
+def luas_segitiga():
+    a = int(input(" Masukan alas segitiga: "))
+    t = int (input("Masukan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segitiga adalah:", luas)
+
+luas_segitiga()
